f135c117d65210403b38b6775516b7843972516f
Narutinn <69373580+Gabriel-bits@users.noreply.github.com>
2022-01-23 13:33:05 -0300

    Fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tracemalloc
 from discord.activity import BaseActivity
 from discord.client import Client
 from discord.ext.commands import AutoShardedBot
-# from secrect import TOKEN, Secret
+from secrect import TOKEN
 from cogs.fuct import load_config
 
 
@@ -53,4 +53,4 @@ if __name__ == "__main__":
         client.load_extension(modulo)
 
 
-client.run(Secret)
+client.run(TOKEN())
